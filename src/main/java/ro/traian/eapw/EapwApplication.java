cb70bbd928a5ce4743a8d5f89d7e1f141974bd6e
Traian Simedru <simedru.traian.pabd24@uab.ro>
2024-10-22 11:46:58 +0300

    :tada: init

---
+++ src/main/java/ro/traian/eapw/EapwApplication.java
@@ -0,0 +1,13 @@
+package ro.traian.eapw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EapwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/traian/eapw/EapwApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.traian.eapw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EapwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
