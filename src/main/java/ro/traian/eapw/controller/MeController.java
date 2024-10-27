19fde3243c95fa94c6b58b866c3ff9327e029bf2
Traian Simedru <simedru.traian.pabd24@uab.ro>
2024-10-27 14:19:10 +0200

    :hammer: fix me service and me controller

--- src/main/java/ro/traian/eapw/controller/MeController.java
+++ src/main/java/ro/traian/eapw/controller/MeController.java
@@ -3,8 +3,11 @@ package ro.traian.eapw.controller;
 import java.security.Principal;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import ro.traian.eapw.dao.me.MeUpdate;
 import ro.traian.eapw.entity.AppUser;
@@ -22,31 +27,46 @@ import ro.traian.eapw.service.me.IMeService;
 @RequestMapping("/api")
 public class MeController {
     private final IMeService meService;
+    private final SecurityContextRepository securityContextRepository = new HttpSessionSecurityContextRepository();
+    private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder
+            .getContextHolderStrategy();
 
     @GetMapping("/me")
     @PreAuthorize("isAuthenticated()")
     public AppUser me(Principal principal) {
-        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
-        String email = loggedInUser.getPrincipal().toString();
-
-        return meService.find(email);
+        return meService.find(principal.getName());
     }
 
     @PatchMapping("/me")
     @PreAuthorize("isAuthenticated()")
-    public AppUser updateMe(@RequestBody MeUpdate meUpdate) {
-        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
-        String email = loggedInUser.getPrincipal().toString();
+    public AppUser updateMe(
+            @RequestBody MeUpdate meUpdate,
+            Principal principal,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        AppUser updatedMe = meService.update(principal.getName(), meUpdate);
 
-        return meService.update(email, meUpdate);
+        if (!updatedMe.getEmail().equals(principal.getName())) {
+            SecurityContext context = securityContextHolderStrategy.createEmptyContext();
+            securityContextHolderStrategy.setContext(context);
+            securityContextRepository.saveContext(context, request, response);
+        }
+
+        return updatedMe;
     }
 
     @DeleteMapping("/me")
     @PreAuthorize("isAuthenticated()")
-    public boolean deleteMe() {
-        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
-        String email = loggedInUser.getPrincipal().toString();
+    public boolean deleteMe(
+            Principal principal,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        boolean result = meService.delete(principal.getName());
 
-        return meService.delete(email);
+        SecurityContext context = securityContextHolderStrategy.createEmptyContext();
+        securityContextHolderStrategy.setContext(context);
+        securityContextRepository.saveContext(context, request, response);
+
+        return result;
     }
 }
